1e2e720cccc8acd2af64568d8703f273c2a08728
vinnyvega <davis.vincent@gmail.com>
2015-06-15 19:11:57 -0500

    Update ReentrantCounter.java

--- q1/ReentrantCounter.java
+++ q1/ReentrantCounter.java
@@ -3,9 +3,10 @@ import java.util.concurrent.locks.ReentrantLock;
 // TODO
 // Use ReentrantLock to protect the count
 public class ReentrantCounter extends Counter {
+    private final ReentrantLock lock = new ReentrantLock();
+    
     @Override
     public void increment() {
-    	ReentrantLock lock = new ReentrantLock();
         lock.lock();  // block until condition holds
         try {
           // ... method body
