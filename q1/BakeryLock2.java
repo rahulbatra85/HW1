f9d45a68c216de978f0a3c970c42472b3f5e393c
Rahul Batra <rahulbatra85@gmail.com>
2015-06-17 23:28:41 -0500

    Reverted to Rahul's version as main one

--- q1/BakeryLock2.java
+++ q1/BakeryLock2.java
@@ -1,49 +1,56 @@
+// TODO
+// Implement the bakery algorithm
+// Remember to use volatile qualifier for shared variables to guarantee atomicity
 import java.util.concurrent.atomic.AtomicIntegerArray;
-import java.util.concurrent.atomic.*;
 
 public class BakeryLock implements MyLock {
-    volatile int N;
-//    volatile boolean[] choosing; // inside doorway
-//    volatile int[] number;
-	AtomicIntegerArray number,choosing;
     
-    public BakeryLock(int numProc) {
-        N = numProc;
-        choosing = new AtomicIntegerArray(N);
-        number = new AtomicIntegerArray(N);
-        for (int j = 0; j < N; j++) {
-            choosing.set(j,0);
-            number.set(j,0);
-        }
-    }
-    
-    @Override
-    public void lock(int tid) {
-        // step 1: doorway: choose a number
-//        choosing[tid] = true;
-        choosing.set(tid,1);
-		int max = 0;
-        for (int j = 0; j < N; j++){
-            if (number.get(j) > max){
-                max = number.get(j);
-			}
-		}
-        number.set(tid, max + 1);
-		choosing.set(tid,0);
+    private volatile int numThread;
+    private volatile boolean[] choosing;  
+    private AtomicIntegerArray queue_num; 
 
-        // step 2: check if my number is the smallest
-        for (int j = 0; j < N; j++) {
-            //while (choosing[j]) ; // process j in doorway
-            while (choosing.get(j) == 1) ; // process j in doorway
-            while ((number.get(j) != 0) &&
-//                    ((number[j] < number[tid]) || ((number[j] == number[tid]) && j < tid)))
-                    ((number.get(j) < number.get(tid)) || ((number.get(j) == number.get(tid)) && j < tid)))
-                ; // busy wait
+
+    //init lock
+    public BakeryLock(int numThread) {
+    
+        this.numThread = numThread;
+        
+        queue_num = new AtomicIntegerArray(numThread); // initialized to 0 by default
+        choosing = new boolean[numThread];
+        
+        //init choosing[] to all false and queue_num[] to all 0s
+        for (int j=0; j<numThread; j++) {
+            choosing[j] = false;
+            //queue_num[j] = 0;
         }
     }
-    
+
     @Override
-    public void unlock(int i) { // exit protocol
-        number.set(i,0);
+    public void lock(int myId) {
+    
+        //doorway -- grab queue_num (+1 from highest)
+        choosing[myId] = true;         
+        for (int j=0; j<numThread; j++) {
+            if ( queue_num.get(j) > queue_num.get(myId) )  
+                queue_num.set(myId, queue_num.get(j));            
+        }               
+        queue_num.incrementAndGet(myId); 
+        choosing[myId] = false;  
+
+    
+        //in queue -- wait for queue_num to be next (smallest from others)
+        for (int j=0; j<numThread; j++) {
+            while (choosing[j]); // wait for thread j to get queue_num
+            while ( (queue_num.get(j)  != 0) && //thread j is done
+                     ( (queue_num.get(j) < queue_num.get(myId)) || //thread j has a smaller queue_num
+                        (queue_num.get(j) == queue_num.get(myId)) && (j < myId)  //check pids
+                      )
+                   ); //wait for other threads to finish    
+        }  
+    }
+
+    @Override
+    public void unlock(int myId) {
+        queue_num.set(myId,0); //done
     }
 }
